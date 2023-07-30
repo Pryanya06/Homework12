f884eaf41397bb581a242a39d58a521f93137f49
Alice <foralice06@mail.ru>
2023-07-30 22:21:10 +0500

    HW12

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Author henry = new Author("О", "Генри");
+        Author henry = new Author("О.", "Генри");
         Author murakami = new Author("Харуки", "Мураками");
 
         Book soulMates = new Book("Родственные души", henry, 1911);
         Book kafka = new Book("Кафка на пляже", murakami, 2002);
 
-        System.out.println(soulMates.getTitle() + ' ' + soulMates.getPublishingYear() + ' ' + soulMates.getAuthor().getFirstName() + ' ' + soulMates.getAuthor().getSecondName());
-        System.out.println(kafka.getTitle() + ' ' + kafka.getPublishingYear() + ' ' + kafka.getAuthor().getFirstName() + ' ' + kafka.getAuthor().getSecondName());
+        printBook(soulMates);
+        printBook(kafka);
+
+        soulMates.setPublishingYear(1910);
+        printBook(soulMates);
+
     }
+
+    private static void printBook(Book soulMates) {
+        System.out.println(soulMates.getTitle() + ' ' + soulMates.getPublishingYear() + ' ' + soulMates.getAuthor().getFirstName() + ' ' + soulMates.getAuthor().getSecondName());
+    }
+
 }
